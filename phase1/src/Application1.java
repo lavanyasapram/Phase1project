9a2dfa74ce81effef50cdbd068c0409229481001
Lavanya <Lavanya@192.168.0.6>
2021-04-11 19:58:14 +0530

    First commit

---
+++ phase1/src/Application1.java
@@ -0,0 +1,12 @@
+import org.example.filedeatils.screens.WelcomeScreen;
+public class Application1 {
+  
+	public static void main(String[] args) {
+    	WelcomeScreen welcome = new WelcomeScreen();
+    	welcome.introWS();
+    	welcome.GetUserInput();
+		// TODO Auto-generated method stub
+
+	}
+
+}
